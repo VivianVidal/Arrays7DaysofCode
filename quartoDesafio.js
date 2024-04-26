cd1f49e89575a92634d0a52549bd37b322033bb8
Vivian <viviancristinavidal@gmail.com>
2024-04-26 19:49:32 -0300

    atualização desafio 4

--- quartoDesafio.js
+++ quartoDesafio.js
@@ -1,12 +1,3 @@
-class Pedido {
-    constructor(numPedido, nomeCliente, itensPedido, valorTotal) {
-        this.numPedido = numPedido;
-        this.nomeCliente = nomeCliente;
-        this.itensPedido = itensPedido;
-        this.valorTotal = valorTotal;
-    }
-}
-
 class FiladePedidos {
     constructor() {
         this.numPedido = []
@@ -62,16 +53,16 @@ class FiladePedidos {
 
 
 let pedido = new FiladePedidos
-//pedido.adicionarPedido(1, "davi", "azeite e batata", 30)
-//pedido.adicionarPedido(2, "heloisa", "cachorro quente", 45)
-//pedido.adicionarPedido(3, "joao", "batata frita", 90)
+pedido.adicionarPedido(1, "davi", "azeite e batata", 30)
+pedido.adicionarPedido(2, "heloisa", "cachorro quente", 45)
+pedido.adicionarPedido(3, "joao", "batata frita", 90)
 //pedido.adicionarPedido(4, "pamela", "azeitona", 100)
 //pedido.adicionarPedido(5, "ana", "lasanha", 60)
 //pedido.adicionarPedido(6, "Vivi", "café", 80)
-//pedido.mostrarPedidos()
-//pedido.removerPedidoEntregue()
+pedido.mostrarPedidos()
+pedido.removerPedidoEntregue()
 //pedido.adicionarPedido(7, "karen", "açucar", 3)
-//pedido.mostrarPedidos()
+pedido.mostrarPedidos()
 //pedido.removerPedidoEntregue()
 //pedido.mostrarPedidos()
 //pedido.cancelarPedido(6)
